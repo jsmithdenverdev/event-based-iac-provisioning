9a8c9612e5415d89af37f70b9119f10e734e1f54
Jacob Smith <jsmithdenverdev@gmail.com>
2022-12-30 23:22:53 -0700

    Removed .net files

--- services/account/Account.Functions/src/Account.Functions/Models/Account.cs
+++
@@ -1,8 +0,0 @@
-namespace Account.Functions.Models;
-
-public class Account
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public Enums.Subscription Subscription { get; set; }
-}
